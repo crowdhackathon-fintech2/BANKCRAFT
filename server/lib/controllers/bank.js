49b0077481d192a397933fa437a9676041239b84
Apostolos Voulkidis <apovtx@gmail.com>
2017-10-21 16:04:38 +0300

    History endpoints

--- server/lib/controllers/bank.js
+++ server/lib/controllers/bank.js
@@ -4,33 +4,13 @@ const moment = require("moment");
 
 module.exports = function () {
     // Fetch all registered device tokens
-    const DESCRS = ["Girlfriend gift", "Tickets", "Vasilopoulos"];
+    const DESCRS = ["Girlfriend gift", "Tickets", "Vasilopoulos", "High Heels"];
     const IBAN= "GR4501101030000010348012377";
     const USER_ID = "d3247c73-2073-4921-847d-b22a51090c45";
     const BANK_ID = "DB173089-A8FE-43F1-8947-F1B2A8699829";
     const ACCOUNT_ID = "762d1a5d-64bb-465f-a0d8-2dd87b95f169";
     const VIEW_ID = "owner";
     const SANDBOX_ID = "bankcraftNBG";
-
-    var options = { method: 'POST',
-        url: `https://apis.nbg.gr/public/nbgapis/obp/v3.0.1/banks/${BANK_ID}/accounts/${ACCOUNT_ID}/${VIEW_ID}/transaction-request-types/sepa/transaction-requests`,
-        headers:
-            { provider: 'NBG',
-                provider_id: 'NBG.gr',
-                username: 'User1',
-                user_id: USER_ID,
-                application_id: SANDBOX_ID,
-                sandbox_id: SANDBOX_ID,
-                accept: 'text/json',
-                'content-type': 'text/json' },
-        body:
-            { to:
-                { iban: 'randomIban' },
-                charge_policy: 'SHARED',
-                value: { currency: 'EUR', amount: Math.random().toFixed(3)+getRandomInt(5,85)},
-                description: getRandomDescription()
-            },
-        json: true };
 
 
     // console.log(getRandomDescription())
@@ -44,6 +24,26 @@ module.exports = function () {
     }
 
     function doTransaction(){
+        var options = { method: 'POST',
+            url: `https://apis.nbg.gr/public/nbgapis/obp/v3.0.1/banks/${BANK_ID}/accounts/${ACCOUNT_ID}/${VIEW_ID}/transaction-request-types/sepa/transaction-requests`,
+            headers:
+                { provider: 'NBG',
+                    provider_id: 'NBG.gr',
+                    username: 'User1',
+                    user_id: USER_ID,
+                    application_id: SANDBOX_ID,
+                    sandbox_id: SANDBOX_ID,
+                    accept: 'text/json',
+                    'content-type': 'text/json' },
+            body:
+                { to:
+                    { iban: 'randomIban' },
+                    charge_policy: 'SHARED',
+                    value: { currency: 'EUR', amount: Math.random().toFixed(3)+getRandomInt(5,85)},
+                    description: getRandomDescription()
+                },
+            json: true };
+
         request(options, function (error, response, body) {
             if (error) return console.error('Failed: %s', error.message);
 
@@ -77,6 +77,32 @@ module.exports = function () {
         for(let i=0; i<numberOfTransactions;i++){
             doTransaction();
         }
+    }
+
+    getTransactionHistory();
+    function getTransactionHistory(){
+        var options = { method: 'GET',
+            url: `https://apis.nbg.gr/public/nbgapis/obp/v3.0.1/my/banks/${BANK_ID}/accounts/${ACCOUNT_ID}/transactions`,
+            headers:
+                { provider: 'NBG',
+                    provider_id: 'NBG.gr',
+                    username: 'User1',
+                    user_id: USER_ID,
+                    application_id: SANDBOX_ID,
+                    sandbox_id: SANDBOX_ID,
+                    accept: 'text/json',
+                    'content-type': 'text/json'
+                },
+            json: true };
+
+        request(options, function(error, response, body){
+            if(error){
+                return console.log('Error retrieving transactions');
+            }
+
+            body.forEach(v=>console.log(v.details.description))
+        });
+
     }
 
     return {
--- server/lib/routes/transactions.js
+++ server/lib/routes/transactions.js
@@ -2,5 +2,7 @@ const db = require('../../util/db')();
 
 module.exports = function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(db.getDatabase());
+    const data = db.getDatabase();
+    data.total = 17+data.transactions.reduce((a,c)=>a=a+c.investedAmount,0)
+    res.send(data);
 };
