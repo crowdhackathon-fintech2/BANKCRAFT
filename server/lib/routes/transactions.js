a44f418e0d6286531b3305248a175af822524ee1
Apostolos Voulkidis <apovtx@gmail.com>
2017-10-22 08:11:39 +0300

    Minor changes

--- server/lib/routes/transactions.js
+++ server/lib/routes/transactions.js
@@ -1,8 +1,12 @@
 const db = require('../../util/db')();
 
 module.exports = function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
     const data = db.getDatabase();
     data.total = 17+data.transactions.reduce((a,c)=>a=a+c.investedAmount,0);
-    res.send(data);
+    if(res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+        return
+    }
+    return data;
 };
--- server/views/app.js
+++ server/views/app.js
@@ -4,6 +4,7 @@
     // Declare app level module which depends on views, and components
     angular.module("winvest", [
         "ngRoute",
+        "ngAnimate",
         'ngWebSocket',
         "home",
     ]).
